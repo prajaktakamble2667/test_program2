5a2e41bbc42fce713bb8e15c5c75f53e6975fb64
prajaktakamble2667 <145985607+prajaktakamble2667@users.noreply.github.com>
2024-02-28 16:30:00 +0530

    Add files via upload

---
+++ username.js
@@ -0,0 +1,11 @@
+
+var userName = prompt("Please enter your name:");
+
+    if (userName !== null) 
+    {
+        alert("Hello, " + userName + "!");
+    }
+    else 
+    {
+    alert("You cancelled the prompt.");
+    }
